9b67ce0097e4503f75672a6c32e39cb7ee796f60
Matt Cowan <cowan@bnl.gov>
2015-06-01 13:45:32 -0400

    added getSampleIDfromAbsoluteDewarPos

--- db_lib.py
+++ db_lib.py
@@ -49,6 +49,8 @@ print "---- connecting with:  mongo.connect({0}, host={1}) ----".format(db_name,
 mongo_conn = mongo.connect(db_name, host=db_host)
 
 
+primaryDewarName = 'primaryDewar2'
+
 
 def createContainer(container_name, type_name, capacity):
     containerObj = {"containerName": container_name,
@@ -258,7 +260,7 @@ def getAllPucks(as_mongo_obj=False):
 
 
 def getPrimaryDewar(as_mongo_obj=False):
-    return getContainerByName("primaryDewar2", as_mongo_obj=as_mongo_obj)
+    return getContainerByName(primaryDewarName, as_mongo_obj=as_mongo_obj)
 
 
 def getContainerByName(container_name, as_mongo_obj=False): 
@@ -308,7 +310,7 @@ def getQueue():
     # try to only retrieve what we need...
     # Use .first() instead of [0] here because when the query returns nothing,
     # .first() returns None while [0] generates an IndexError
-    items = Container.objects(containerName='primaryDewar2').only('item_list').get()
+    items = Container.objects(containerName=primaryDewarName).only('item_list').get()
     for item_id in items.item_list:
         if item_id is not None:
             puck = Container.objects(container_id=item_id).only('item_list').get()
@@ -325,7 +327,7 @@ def getQueue():
 
 
 def getDewarPosfromSampleID(sample_id):
-    cont = Container.objects(containerName='primaryDewar2').only('item_list').first()
+    cont = Container.objects(containerName=primaryDewarName).only('item_list').first()
     if cont is not None:
         for puck_id in cont.item_list:
             if puck_id is not None:
@@ -339,7 +341,7 @@ def getDewarPosfromSampleID(sample_id):
 
 
 def getAbsoluteDewarPosfromSampleID(sample_id):
-    cont = Container.objects(containerName="primaryDewar2").only('item_list').first()
+    cont = Container.objects(containerName=primaryDewarName).only('item_list').first()
     if cont is not None:
         for i,puck_id in enumerate(cont.item_list):
             if puck_id is not None:
@@ -348,9 +350,49 @@ def getAbsoluteDewarPosfromSampleID(sample_id):
                 puckCapacity = len(sampleList)  # would be more efficient to have a capacity field
     
                 for j,samp_id in enumerate(sampleList):
-                    if samp_id is not None and samp_id == sample_id:
+                    if samp_id == sample_id and samp_id is not None:
+                        # hrrmmmm... wanted to change this to something with += j
+                        # to handle different capacity pucks, but absoluteposition
+                        # doesn't work with variable capacity pucks *and* possibility
+                        # of empty puck positions within dewar :(
                         absPosition = (i*puckCapacity) + j
                         return absPosition
+
+
+def getSampleIDfromAbsoluteDewarPos(abs_pos):
+    # try/except is faster than checking for an empty list before 
+    # indexing into it!
+    try:
+        pd = Container.objects(containerName=primaryDewarName).only('item_list')[0]
+    except IndexError:
+        raise IndexError('Failed to find container named: "{0}"'.format(primaryDewarName))
+
+    # get puck capacity from the 1st puck we find
+    # This is wonky, need to totally rethink the idea of absolute position.
+    for i,puck_id in enumerate(pd.item_list):
+        if puck_id is not None:
+            #puck = getContainerByID(puck_id)
+            try:
+                puck = Container.objects(container_id=puck_id).only('item_list')[0]
+            except IndexError:
+                raise IndexError('Failed to find container_id: "{0}"'.format(puck_id))
+
+            puck_capacity = len(puck.item_list)
+            break
+
+    # get puck# and position from absolute position
+    (puck_num, position) = divmod(abs_pos, puck_capacity)
+
+    # use puck# and position to get the sample_id
+    puck_id = pd.item_list[puck_num]
+    try:
+        puck = Container.objects(container_id=puck_id).only('item_list')[0]
+    except IndexError:
+        if puck_id is None:
+            raise ValueError('no puck in position {0} in primary dewar!'.format(puck_num))
+        raise IndexError('Failed to get puck id {0}.'.format(puck_id))
+
+    return puck.item_list[position]
 
 
 def popNextRequest():
