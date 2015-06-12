e25ea1a69e7364bdcc23d08542eb0d231804c5ef
Matt Cowan <cowan@bnl.gov>
2015-06-12 09:14:44 -0400

    cleanup and cant currently enforce request_id unique :(

--- odm_templates.py
+++ odm_templates.py
@@ -11,32 +11,24 @@ if LooseVersion(mongoengine.__version__) >= LooseVersion('0.7.0'):
 
 
 class Container(DynamicDocument):
-    container_id = SequenceField(#collection_name='mongoengine.counters',
-                                 #sequence_name='container',
-                                 required=True, unique=True, **args_dict)
+    container_id = SequenceField(required=True, unique=True, **args_dict)
 
 class Request(DynamicEmbeddedDocument):
-    request_id = SequenceField(#collection_name='mongoengine.counters',
-                               #sequence_name='request',
-                               required=True, unique=True, **args_dict)
-    # unique doesn't seem to be enforced!?
+    # doh!  mongo doesn't currently enforce uniqueness
+    # of embedded doc fields in a list field :(
+    request_id = SequenceField(required=True, unique=True, **args_dict)
+        
 
 class Sample(DynamicDocument):
-    sample_id = SequenceField(#collection_name='mongoengine.counters',
-                              #sequence_name='sample',
-                              required=True, unique=True, **args_dict)
+    sample_id = SequenceField(required=True, unique=True, **args_dict)
     requestList = ListField(EmbeddedDocumentField(Request))
-#    sampleName = StringField(required=True, unique_with='owner')
-    # unique on request_id doesn't seem to be enforced even trying this
-    meta = {'indexes': [
-                       {'fields': {'requestList.request_id': 1},
-                        'sparse': True, 'unique': True}
-            ]}
+    #owner = StringField(required=True, unique_with='owner')
+    #owner = StringField()
+    #sampleName = StringField(required=True, unique_with='owner')
+    sampleName = StringField(required=True)
 
 class Raster(DynamicDocument):
-    raster_id = SequenceField(#collection_name='mongoengine.counters',
-                              #sequence_name='raster',
-                              required=True, unique=True, **args_dict)
+    raster_id = SequenceField(required=True, unique=True, **args_dict)
 
 
 class BeamlineInfo(DynamicDocument):
