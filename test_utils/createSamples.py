97ed567e1e2d151263ea1a01efa1f91a9567b9fb
Matt Cowan <cowan@bnl.gov>
2015-06-12 09:13:56 -0400

    better debugging for duplicate issues

--- test_utils/createSamples.py
+++ test_utils/createSamples.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 
-from db_lib import *
 import time
+import mongoengine
+
+from db_lib import *
+
 
 for i in range (1,25):
   containerName = "Puck" + str(i)
   for j in range (0,16):
     sampleName = "samp_"+str(i)+"_"+str(j)
-#    time.sleep(1)
-    sampID = createSample(sampleName)
-    insertIntoContainer(containerName,j,sampID)
 
+    try:
+      sampID = createSample(sampleName)
+    except mongoengine.NotUniqueError:
+      raise mongoengine.NotUniqueError('{0}'.format(sampleName))
 
+    insertIntoContainer(containerName, j, sampID)
