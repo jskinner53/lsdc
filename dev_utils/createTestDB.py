ce12d015f59e256833adacdb6557c5adc720fd29
Matt Cowan <cowan@bnl.gov>
2015-10-16 16:46:41 -0400

    blar, what happened here?

--- dev_utils/createTestDB.py
+++ dev_utils/createTestDB.py
@@ -29,23 +29,23 @@ def createTestDB():
                       {'name': 'capacity',
                        'description': 'postitive integer, number of sample positions within the container',
                        'bson_type': '64-bit integer?'}]
-        
+
         for f in  base_fields:
             print('name: {0}'.format(f['name']))
             createField(f['name'], f['description'], f['bson_type'])
-        
-    
+
+
         # base types
         parent_types = [{'name': 'sample', 'description': '', 'parent_type': 'base'},
                       {'name': 'container', 'description': '', 'parent_type': 'base'},
                       {'name': 'request', 'description': '', 'parent_type': 'base'},
                       {'name': 'result', 'description': '', 'parent_type': 'base'}]  # 'location'?
-        
+
         for t in  parent_types:
             print('name: {0}'.format(t['name']))
             createType(t['name'], t['description'], t['parent_type'])
-        
-    
+
+
         types = [{'name': 'puck',
                   'description': 'generic hockey puck style subcontainer for shipping dewars',
                   'parent_type': 'container'},
@@ -55,12 +55,12 @@ def createTestDB():
                  {'name': 'shipping_dewar', 'description': '', 'parent_type': 'dewar'},
                  {'name': 'test_request', 'description': '', 'parent_type': 'request'},
                  {'name': 'test_result', 'description': '', 'parent_type': 'result'}]
-        
+
         for t in types:
             print('name: {0}, parent_type {1}'.format(t['name'], t['parent_type']))
             createType(t['name'], t['description'], t['parent_type'])
-    
-    
+
+
         # containers with a fixed number of sample or subcontainer locations
         types = [{'name': '16_pin_puck',
                   'description': 'original 16 pin puck',
@@ -71,7 +71,7 @@ def createTestDB():
                  {'name': '5_slot_cane',
                   'description': 'traditional, 5 position cane for pins in vials',
                   'parent_type': 'container', 'capacity': 5}]
-        
+
         for t in types:
             print('name: {0}, parent_type {1}'.format(t['name'], t['parent_type']))
             createType(t['name'], t['description'], t['parent_type'], capacity=t['capacity'])
@@ -93,39 +93,39 @@ def createTestDB():
         for i in range(1,5)+[7]:  # 1 indexed, discontinuity for testing
             containerName = 'Puck_{0}'.format(i)
             createContainer(containerName, '16_pin_puck')
-        
+
         for i in range(1,5):  # discontinuity for testing
             containerName = 'dewar_{0}'.format(i)
             createContainer(containerName, 'shipping_dewar')
-        
-        
+
+
         # named containers
         primary_dewar_name = 'primaryDewar'
-        
-        createContainer(primary_dewar_name, '24_puck_robot_dewar') 
-        
+
+        createContainer(primary_dewar_name, '24_puck_robot_dewar')
+
         for i in range(1,5)+[7]:  # discontinuity for testing
             containerName = 'Puck_{0}'.format(i)
             insertIntoContainer(primary_dewar_name, i, getContainerIDbyName(containerName))
-        
-        
+
+
         # samples
         type_name = 'pin'
         for i in range(1,4)+[7]:  # discontinuity for testing
             containerName = 'Puck_{0}'.format(i)
             for j in range(1,5)+[7]:
                 sampleName = 'samp_{0}_{1}'.format(i, j)
-        
+
                 try:
                     sampID = createSample(sampleName, sample_type=type_name)
 
                 except NotUniqueError:
                     raise NotUniqueError('{0}'.format(sampleName))
-        
+
                 if not insertIntoContainer(containerName, j, sampID):
                     print('name {0}, pos {1}, sampid {2}'.format(containerName, j, sampID))
-        
-        
+
+
         # bare requests
 #        request_type = 'test_request'
 #        request_id = createRequest(request_type,
@@ -136,7 +136,7 @@ def createTestDB():
 #        result_type = 'test_result'
 #        createResult(result_type, request_id,
 #                      {'test_result_value': 'bare result 1'})
-        
+
         # in requestList on sample
 
 #        request_id = addRequesttoSample(sampID,
@@ -153,7 +153,10 @@ def createTestDB():
         # in resultsList on sample
 #        addResultforRequest(result_type, request_id,
 #                             {'test_result_val': 'test val 1'})
-        beamlineInfo('john', 'mountedSample', info_dict={'puckPos':0,'pinPos':0,'sampleID':-99})
+
+        beamlineInfo('john', 'mountedSample', info_dict={'puckPos': 0, 'pinPos': 0,
+                                                         'sampleID': -99})
+
 
     except Exception as e:
         print('Warning! caught exception, dropping incomplete db!\n\n{0}\n'.format(e))
